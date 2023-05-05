0d0848ed8ffa95566ba9ce040234c38bcf426373
RZorkin <zorkin81@gmail.com>
2023-05-05 18:17:29 +0300

    Add inform

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,7 +8,7 @@ function getRandomHexColor() {
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title ? <h2 className={css.title}>{title}</h2> : null}
+       {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map(stat => (
